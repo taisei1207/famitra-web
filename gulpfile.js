89b9b0a0c71a692d4271ac33db7e90982f3f14d9
taisei <taisei.radio.1207@gmail.com>
2024-01-25 16:35:20 +0900

    gulp 修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
+var sass = require('gulp-sass');
 var sassGlob = require('gulp-sass-glob');
 var sourcemaps = require('gulp-sourcemaps');
 var plumber = require('gulp-plumber');
